3618c682663b3d9f72d7bff26cc8bf1870d1fe0e
luannfirme <luannfirme@gmail.com>
2023-11-13 09:43:34 -0300

    Update Semana 1

---
+++ Semana 1/Aula/Program.cs
@@ -0,0 +1,16 @@
+﻿#region tipo de dados
+
+ int tipoInteiro;
+ double tipoDouble;
+ bool tipoBooleano;
+ string tipoTexto;
+ long tipoInteiroLongo;
+
+ tipoInteiro = int.MaxValue;
+ tipoInteiroLongo = long.MaxValue;
+ tipoDouble = double.MaxValue;
+
+ Console.WriteLine(tipoInteiro);
+ Console.WriteLine(tipoInteiroLongo);
+
+#endregion

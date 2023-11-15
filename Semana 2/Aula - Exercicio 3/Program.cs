46229019a2d8d566b0b1afab9d38a56b738fb44c
luannfirme <luannfirme@gmail.com>
2023-11-15 11:53:47 -0300

    Aula 14/11/2023

---
+++ Semana 2/Aula - Exercicio 3/Program.cs
@@ -0,0 +1,38 @@
+﻿#region Exercicio 1
+
+var limite = 30;
+var valor = 1;
+
+Console.WriteLine("Divisores:");
+
+while(valor >= 1  && valor <= limite){
+
+    if (valor % 3 == 0)
+        Console.WriteLine($"O valor {valor} e divisivel por 3.");
+
+    if (valor % 4 == 0)
+        Console.WriteLine($"O valor {valor} e divisivel por 4.");    
+
+    valor++;
+}
+
+#endregion
+
+#region Exercicio 2
+
+var fib1 = 0;
+var fib2 = 1;
+var fib3 = 0;
+
+Console.WriteLine("Fibonacci:");
+
+do {
+    Console.WriteLine(fib3);
+    fib3 = fib2;
+    fib1 =  fib2 + fib3;
+    fib2 = fib1+fib3;
+} while(fib3 <= 100);
+
+
+
+#endregion
---
+++ Semana 2/Exercicio 1/Program.cs
@@ -0,0 +1,9 @@
+﻿#region Exemplo Vector
+
+string[] colecao= { "Item1", "Item2", "Item3", "Item4" };
+foreach (string item in colecao)
+{
+ Console.WriteLine(item );
+}
+
+#endregion

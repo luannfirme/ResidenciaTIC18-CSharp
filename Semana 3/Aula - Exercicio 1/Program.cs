89f43336a6d2a6abbc78e81365e444a396d1e737
luannfirme <luannfirme@gmail.com>
2023-11-22 09:52:42 -0300

    ajuste

--- Semana 3/Aula - Exercicio 1/Program.cs
+++ Semana 3/Aula - Exercicio 1/Program.cs
@@ -3,6 +3,6 @@
 string[] pessoas = {"Luann", "Camila", "Theo", "Laura"};
 char letra = 'a';
 
-Console.WriteLine($"Pessoas que possuem a letra {letra}: {string.Join(", ", pessoas.Where(p => p.Contains(letra)))}");
+Console.WriteLine($"Pessoas que possuem a letra {letra}: {string.Join(", ",pessoas.Where(p => p.Contains(letra)))}");
 
 #endregion

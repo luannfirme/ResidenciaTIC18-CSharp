ae1af8b4e0c3dd9fa4c05ff1bca40c08a4346dc3
luannfirme <luannfirme@gmail.com>
2023-12-04 00:09:03 -0300

    alguma validações

--- Semana 4/Atividade-Grupo/Model/Medico.cs
+++ Semana 4/Atividade-Grupo/Model/Medico.cs
@@ -3,4 +3,26 @@
 public class Medico : Pessoa
 {
     public string Crm { get; set; }
+
+    public override string ValidadarCPF(List<string> cpf)
+    {
+        if(Cpf.Length < 11)
+            return "CPF incompleto";
+
+        var cpfExistente = cpf.Where(c => c == Cpf);
+
+        if(cpfExistente.Any() || cpfExistente != null)
+            return "CPF já cadastrado";
+
+        return null;
+    }
+
+    public bool ValidarCrm(List<Medico> medicos){
+        var crmExistente = medicos.Where(m => m.Crm == Crm);
+
+        if (crmExistente.Any())
+            return false;
+
+        return true;
+    }
 }

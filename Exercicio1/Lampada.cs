e0427905a0becd20ec6e932b5544a175cd9a1834
luannfirme <luannfirme@gmail.com>
2024-03-12 16:54:22 -0300

    initial

---
+++ Exercicio1/Lampada.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercicio1
+{
+    public class Lampada
+    {
+        public bool Ligada { get; private set; }
+
+        public void Ligar() { this.Ligada = true; }
+
+        public void Desligar() { this.Ligada = false; }
+
+        public void Imprimir()
+        {
+            if (this.Ligada)
+                Console.WriteLine("Lâmpada ligada");
+            else
+                Console.WriteLine("Lâmpada desligada");
+        }
+    }
+}
---
+++ Exercicio1/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using Exercicio1;
+using System.Net;
+
+Lampada l = new Lampada();
+
+Console.WriteLine("Lâmpada inicializada");
+
+void Menu()
+{
+    Console.WriteLine("");
+    Console.WriteLine("1 - Ligar");
+    Console.WriteLine("2 - Desligar");
+    Console.WriteLine("3 - Imprimir");
+    Console.WriteLine("0 - Sair");
+    Console.Write("R:");
+
+    int r = int.Parse(Console.ReadLine());
+
+    do
+    {
+        switch (r)
+        {
+            case 1:
+                l.Ligar();
+                Menu();
+                break;
+            case 2:
+                l.Desligar();
+                Menu();
+                break;
+            case 3:
+                Console.WriteLine("");
+                l.Imprimir();
+                Menu();
+                break;
+            case 0:
+                Console.WriteLine("");
+                Console.WriteLine("Aplicação encerrada...");
+                Console.WriteLine("");
+                return;
+            default:
+                Menu();
+                break;
+        }
+
+    } while (r == 0);
+
+}
+
+Menu();
+
+

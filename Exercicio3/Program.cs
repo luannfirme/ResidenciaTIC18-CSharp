09e470117e85f1484d662f0a33c428d51ba7bb5b
Luann Firme <luann.firme@daten.com.br>
2024-03-15 20:37:10 -0300

    Exercicio 3

---
+++ Exercicio3/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    private enum Exercicio
+    {
+        Academia = 1,
+        Luta = 2,
+        Corrida = 3
+    }
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Selecione um exercício:");
+
+        foreach (Exercicio exercicio in Enum.GetValues(typeof(Exercicio)))
+        {
+            Console.WriteLine($"{(int)exercicio} - {exercicio}");
+        }
+
+        Console.WriteLine("");
+        Console.Write("Digite o número correspondente ao exercício desejado: ");
+        try
+        {
+            int opcao = int.Parse(Console.ReadLine());
+            if (Enum.IsDefined(typeof(Exercicio), opcao))
+            {
+                Exercicio exercicioEscolhido = (Exercicio)opcao;
+                Console.WriteLine($"Você escolheu: {exercicioEscolhido}");
+            }
+            else
+            {
+                Console.WriteLine("Opção inválida.");
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Formato inválido. Por favor, insira um número.");
+        }
+    }
+}
+
